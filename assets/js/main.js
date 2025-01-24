8dab0aae61a783f149b5bea50ef04301eef62162
Mai Tri Thanh <56778099+maitrithanh@users.noreply.github.com>
2025-01-24 20:49:59 +0700

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const btnDestLaos = document.querySelector(".dest-laos");
+  const btnDestVietnam = document.querySelector(".dest-vietnam");
+  const btnDestCambodia = document.querySelector(".dest-cambodia");
+
+  if(btnDestLaos || btnDestVietnam || btnDestCambodia) {
+    btnDestLaos.addEventListener('click', () => {
+      handleRedirectHotelDest("laos");
+    })
+    btnDestVietnam.addEventListener('click', () => {
+      handleRedirectHotelDest("vietnam");
+    })
+    btnDestCambodia.addEventListener('click', () => {
+      handleRedirectHotelDest("cambodia");
+    })
+  }
+
+  const handleRedirectHotelDest = (dest) => {
+    window.location.href = `/hotel/hotel-agent?dest=${dest}`
+  }
+})
