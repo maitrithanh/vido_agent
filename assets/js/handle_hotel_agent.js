b1eec066cb1e5489abac248433b7181a5f37044a
Mai Tri Thanh <maitrithanh06@gmail.com>
2025-02-03 14:44:33 +0700

    layout vido agent

---
+++ assets/js/handle_hotel_agent.js
@@ -0,0 +1,19 @@
+const handleSelectHotel = () => {
+  window.location.href = "/pages/hotel_detail.html";
+};
+
+const handleBookingHotel = () => {
+  window.location.href = "/pages/hotel_confirm.html";
+};
+
+const handleBack = () => {
+  window.history.back();
+};
+
+const handleFinishHotel = () => {
+  window.location.href = "/pages/booking_success.html";
+};
+
+const handleReBookingHotel = () => {
+  window.location.href = "/";
+};
